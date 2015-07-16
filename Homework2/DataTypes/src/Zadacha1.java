acafccf782da55d358f50ed47e3586169fd327f5
milahristianova <mila_hristianovaa@abv.bg>
2015-07-16 20:15:18 +0300

    Update homework1

---
+++ Homework2/DataTypes/src/Zadacha1.java
@@ -0,0 +1,28 @@
+
+public class Zadacha1 {
+//
+	public static void main(String[] args) {
+	float x = 6.5f;
+		System.out.print("x= ");
+		System.out.println(x);
+	
+	float y = 3.2f;
+	System.out.print("y= ");
+	System.out.println(y);
+	
+	 
+	float d = (float) x * y;
+	System.out.print("floating point x * y = " );
+	System.out.println(d);
+	
+	
+
+	
+
+			
+		
+				
+
+	}
+
+}
